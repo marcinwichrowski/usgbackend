d1ea3f4d0f43ca322a175bbde41f9b155759460d
Mati <mati@pjwstk.edu.pl>
2017-07-24 13:03:08 +0200

    initial commit

---
+++ USG_backend_console/USG_backend_console/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace USG_backend_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
